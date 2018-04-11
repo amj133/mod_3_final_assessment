328474b2b23b19bceb871cea4e6ba9747ddb99d9
amj133 <amj@vt.edu>
2018-04-11 11:03:21 -0600

    user can view most dangerous spec passing

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,9 +1,9 @@
 class MostDangerousDayController < ApplicationController
   def index
-    @start_date = params["start_date"]
-    @end_date = params["end_date"]
     start = DateTime.strptime(params["start_date"], '%d %B, %Y')
     finish = DateTime.strptime(params["end_date"], '%d %B, %Y')
+    @start_date = start.strftime("%B %-d, %Y")
+    @end_date = finish.strftime("%B %-d, %Y")
     start = start.strftime('%Y-%m-%d')
     finish = finish.strftime('%Y-%m-%d')
 
@@ -18,23 +18,26 @@ class MostDangerousDayController < ApplicationController
     response = conn.get("/neo/rest/v1/feed", params)
     response = JSON.parse(response.body)
 
+
     date_and_count = Hash.new(0)
     response["near_earth_objects"].each do |date, asteroids|
       asteroids.each do |asteroid|
         if asteroid["is_potentially_hazardous_asteroid"] == true
           date_and_count[date] += 1
-          Asteroid.new(asteroid["name"], asteroid["neo_reference_id"])
         end
       end
     end
 
-    most_dangerous = DateTime.strptime(date_and_count.sort_by {|k, v| v}.last[0], '%Y-%m-%d')
-    @most_dangerous_day = most_dangerous.strftime('%d %B, %Y')
+    most_dangerous_date = date_and_count.sort_by {|k, v| v}.last[0]
+    most_dangerous_datetime = DateTime.strptime(most_dangerous_date, '%Y-%m-%d')
+    @most_dangerous_day = most_dangerous_datetime.strftime('%B %-d, %Y')
 
-
-
-
-      # date is key -> array of asteroids as values
+    @asteroids = []
+    response["near_earth_objects"][most_dangerous_date].each do |asteroid|
+      if asteroid["is_potentially_hazardous_asteroid"] == true
+        @asteroids << Asteroid.new(asteroid["name"], asteroid["neo_reference_id"])
+      end
+    end
   end
 
 end
