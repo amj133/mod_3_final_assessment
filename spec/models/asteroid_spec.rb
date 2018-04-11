8114ef6042d996a86958461cc20ceb12a9c0160f
amj133 <amj@vt.edu>
2018-04-11 11:38:45 -0600

    update asteroid spec per hazardous attr

--- spec/models/asteroid_spec.rb
+++ spec/models/asteroid_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe Asteroid do
   let(:name) {"bob"}
   let(:id) {"45A"}
-  subject {Asteroid.new(name, id) }
+  let(:hazardous) {true}
+  subject {Asteroid.new(name, id, hazardous) }
 
   it "get attributes and sets them as reader methos" do
     expect(subject.name).to eq("bob")
     expect(subject.neo_reference_id).to eq("45A")
+    expect(subject.hazardous).to eq(true)
   end
 end
