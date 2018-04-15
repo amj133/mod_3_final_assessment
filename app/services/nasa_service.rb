1ba5e7e18943c561c3b8f7414d665e4c30b5b909
amj133 <amj@vt.edu>
2018-04-15 17:29:35 -0600

    create asteroids by date method on nasa service - spec passing

--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -7,41 +7,35 @@ class NasaService
   end
 
   def asteroid_by_id
-    json_response("/neo/rest/v1/neo/#{neo_reference_id}")
+    json_response("/neo/rest/v1/neo/#{neo_reference_id}", base_params)
   end
 
   def asteroids_by_date
-    # this belongs in wrapper class
-    date_params = {"start_date" => start_date, "end_date" => end_date}
-    response = json_response("/neo/rest/v1/feed", base_params.merge!(date_params))
-    date = find_most_dangerous_day(response)
-    { date: create_asteroids }
+    response = json_response("/neo/rest/v1/feed", date_params)
   end
 
 
   private
-    attr_reader :neo_reference_id
+    attr_reader :neo_reference_id, :start_date, :end_date
 
     def conn
       Faraday.new(:url => "https://api.nasa.gov/")
     end
 
     def base_params
-      {
-        "api_key" => ENV['NASA_API_KEY']
-      }
+      { "api_key" => ENV['NASA_API_KEY'] }
     end
 
-    def json_response(uri)
-      response = conn.get(uri, base_params)
+    def json_response(uri, params)
+      response = conn.get(uri, params)
       response = JSON.parse(response.body)
     end
 
-    def find_most_dangerous_day(response)
-      # enter earth-shatteringly (pun intended) awesome & refactored code
+    def date_params
+      base_params.merge!({
+        "start_date" => start_date,
+        "end_date" => end_date
+      })
     end
 
-    def create_asteroids
-      # refactored code
-    end
 end
--- spec/services/nasa_service_spec.rb
+++ spec/services/nasa_service_spec.rb
@@ -4,15 +4,24 @@ describe NasaService do
   describe "insstance methods" do
     context "#asteroids_by_date" do
       it "returns raw collection of asteroids" do
-        VCR.use_cassette("WQP service station by huc search") do
+        VCR.use_cassette("Nasa Service Asteroids By Date Search") do
           search_params = {
-            "start_date" => "2018-01-01",
-            "end_date" => "2018-01-07"
+            start_date: "2018-01-01",
+            end_date: "2018-01-07"
           }
 
           raw_asteroids = NasaService.new(search_params).asteroids_by_date
 
           expect(raw_asteroids).to be_a(Hash)
+
+          expect(raw_asteroids["near_earth_objects"]).to be_a(Hash)
+          expect(raw_asteroids["near_earth_objects"]).to have_key("2018-01-01")
+          expect(raw_asteroids["near_earth_objects"]).to have_key("2018-01-07")
+
+          expect(raw_asteroids["near_earth_objects"]["2018-01-07"]).to be_a(Array)
+          expect(raw_asteroids["near_earth_objects"]["2018-01-07"][0]).to be_a(Hash)
+          expect(raw_asteroids["near_earth_objects"]["2018-01-07"][0]).to have_key("is_potentially_hazardous_asteroid")
+          expect(raw_asteroids["near_earth_objects"]["2018-01-07"][0]["is_potentially_hazardous_asteroid"]).to eq(false)
         end
       end
     end
