602f04f8b3d49516548483eebb8aaffbf018fcc3
amj133 <amj@vt.edu>
2018-04-16 04:59:02 -0600

    refactor dates and dangerous asteroids method of most danger day

--- app/services/most_dangerous_day.rb
+++ app/services/most_dangerous_day.rb
@@ -7,8 +7,7 @@ class MostDangerousDay
 
   def asteroids
     response = NasaService.new(search_params).asteroids_by_date
-    date = most_dangerous_date(response)
-    {date => asteroids_for_most_dangerous_date(response, date)}
+    most_dangerous_date_and_asteroids(response)
   end
 
   private
@@ -21,30 +20,21 @@ class MostDangerousDay
       }
     end
 
-    def dates_and_asteroid_count(response)
-      date_and_count = Hash.new(0)
-      response["near_earth_objects"].each do |date, asteroids|
-        asteroids.each do |asteroid|
+    def dates_and_dangerous_asteroids(response)
+      date_and_count = Hash.new([])
+      response["near_earth_objects"].map do |date, asteroids|
+        date_and_count[date] = asteroids.map do |asteroid|
           if asteroid["is_potentially_hazardous_asteroid"] == true
-            date_and_count[date] += 1
+            Asteroid.new(asteroid["name"], asteroid["neo_reference_id"])
           end
-        end
+        end.compact
       end
       date_and_count
     end
 
-    def most_dangerous_date(response)
-      dates_and_asteroid_count(response).sort_by {|k, v| v}.last[0]
-    end
-
-    def asteroids_for_most_dangerous_date(response, date)
-      asteroids = []
-      response["near_earth_objects"][date].each do |asteroid|
-        if asteroid["is_potentially_hazardous_asteroid"] == true
-          asteroids << Asteroid.new(asteroid["name"], asteroid["neo_reference_id"])
-        end
-      end
-      asteroids
+    def most_dangerous_date_and_asteroids(response)
+      sorted = dates_and_dangerous_asteroids(response).sort_by {|k, v| v.count}
+      { sorted.last[0] => sorted.last[1] }
     end
 
 end
